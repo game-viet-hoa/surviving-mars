757829d954711e24d0d92758e3047456b586a581
Xuân Khánh <xuankhanh3393@gmail.com>
2018-03-16 19:05:40 +0700

    Upload mod

---
+++ items.lua
@@ -0,0 +1,7 @@
+return {
+PlaceObj('ModItemLocTable', {
+	'name', "Vietnamese Localization",
+	'language', "English",
+	'filename', "Vietnamese.csv",
+}),
+}
---
+++ metadata.lua
@@ -0,0 +1,16 @@
+return PlaceObj('ModDef', {
+	'title', "Việt Hóa - Vietnamese Localization",
+	'description', "1",
+	'tags', "Vietnamese, Localization",
+	'id', "JVP1OC4",
+	'author', "XKVNN",
+	'version', 1,
+	'lua_revision', 227831,
+	'loctables', {
+		{
+			filename = "Vietnamese.csv",
+			language = "English",
+		},
+	},
+	'saved', 1521200232,
+})
